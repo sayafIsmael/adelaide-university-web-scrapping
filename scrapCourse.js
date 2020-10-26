b112da445fc2c87c55c2010eed3758ff2faec794
Sayaf <bdsayaf@gmail.com>
2020-10-26 14:47:39 +0600

    working on data modeling

--- scrapCourse.js
+++ scrapCourse.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 const scrapCourseDetails = require('./scrapCourseDetails');
 const scrapurl = "https://www.adelaide.edu.au/course-outlines/ug/aborig/";
 
-// module.exports.fetchCourseDetails = async (scrapurl, callback) => {
-(async () => {
+module.exports.fetchCourseDetails = async (scrapurl, callback) => {
+    // (async () => {
 
     try {
         const browser = await puppeteer.launch({ headless: true });
@@ -14,6 +14,7 @@ const scrapurl = "https://www.adelaide.edu.au/course-outlines/ug/aborig/";
         let course = await page.$('div[class="ui-widget-search-results"] > ul');
         let year = await course.$eval('h2', h2 => h2.innerHTML)
 
+        const courseData = []
         if (year.includes(2020)) {
             const courses = await course.$$('li > a');
             for (const course of courses) {
@@ -21,14 +22,17 @@ const scrapurl = "https://www.adelaide.edu.au/course-outlines/ug/aborig/";
                 const courseLink = await link.jsonValue();
                 // console.log({ courseLink });
 
-                let courseData = await scrapCourseDetails.fetchCourseDetails(courseLink);
-                console.log("Course Data Array: ", courseData);
+                let courseDetails = await scrapCourseDetails.fetchCourseDetails(courseLink);
+                courseData.push(courseDetails)
+                // console.log("Course Data Array: ", courseData);
             }
         }
-
+        
         await browser.close()
+        return courseData
+        
     } catch (error) {
         console.log(error)
     }
-})();
-// }
+    // })();
+}
--- scrapCourseDetails.js
+++ scrapCourseDetails.js
@@ -13,7 +13,7 @@ module.exports.fetchCourseDetails = async (scrapurl) => {
             let course = await page.$('table[class="light"] > tbody')
             let courseDetails = await course.$$('tr')
 
-            console.log(courseDetails.length)
+            // console.log(courseDetails.length)
             const courseData = [] 
 
             for (const courseDetail of courseDetails) {
@@ -30,4 +30,87 @@ module.exports.fetchCourseDetails = async (scrapurl) => {
             console.log(error)
         }
     // })();
+}
+
+const model = {
+    courseId: "CDU001",
+    courseName: "Bachelor of Accounting (WACC01 - 2021)",
+    courseCode: "590251",
+    cricosCode: "060230M",
+    studyArea: "Accounting",
+    courseLevel: "Undergraduate",
+    courseStudyModes: [
+        {
+            "studyMode": "Full-time",
+            "duration": "3 year/s"
+        }
+    ],
+    totalCreditPoints: "240",
+    courseUnits: [
+        {
+            "unitType": "Core Units (16 units)",
+            "creditPoints": "160 cp",
+            "description": "",
+            "unitList": [
+                {
+                    "code": "ACT102",
+                    "title": "ACT102 - Introduction to Accounting",
+                    "year": "2020",
+                    "hours": null,
+                    "creditPoints": "10",
+                    "semester": [
+                        {
+                            "year": "2020",
+                            "semester": "Semester 1",
+                            "attendanceMode": "Internal",
+                            "location": "CDU Sydney",
+                            "learningMethod": "OLR"
+                        }
+                    ],
+                    "sector": "Higher Education",
+                    "discipline": "Accounting and Finance",
+                    "prerequisites": "NA",
+                    "incompatible": "ACT101, CMA101",
+                    "assumedKnowledge": "NA",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "unitType": "Specialist Elective (0 units)",
+            "creditPoints": "0 cp",
+            "description": "Specialist Elective units are not required for this course.",
+            "unitList": []
+        },
+        {
+            "unitType": "Electives (6 units)",
+            "creditPoints": "60 cp",
+            "description": "Units totalling 60 credit points selected from undergraduate units offered by the University. When selecting electives choose units at a suitable level. A maximum of 20 credit points may be selected at 100 level as this course includes 80 credit points of compulsory 100 level units. Undergraduate units are coded at 100 to 300 level, therefore units coded as ACT1xx, ACT2xx and ACT3xx would be first, second and third year units respectively.",
+            "unitList": []
+        }
+    ],
+    isAvailableOnline: true,
+    campuses: [
+        {
+            "type": "Offline",
+            "campusName": "CDU Waterfront Darwin (CSP)",
+            "postalAddress": null,
+            "state": null,
+            "geolocation": {
+                "lat": null,
+                "lan": null
+            }
+        },
+    ],
+    courseFees: [
+        {
+            "type": "Annual Fee",
+            "amount": "$26,776.00",
+            "studenType": "International"
+        }
+    ],
+    institutionSpecificData: {
+        "CDUCode": "WACC01"
+    },
+    courseLink: "https://www.cdu.edu.au/study/bachelor-accounting-wacc01-2021"
 }
