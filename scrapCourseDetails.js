9ba7209bf97ea674ceae3838c8d2b680d737a3cd
Sayaf <bdsayaf@gmail.com>
2020-10-26 15:31:21 +0600

    data structure done

--- scrapCourseDetails.js
+++ scrapCourseDetails.js
@@ -1,99 +1,99 @@
 const puppeteer = require('puppeteer');
+const uid = require('uid')
 // const scrapurl = 'https://www.adelaide.edu.au/course-outlines/107654/1/sem-1/';
 
 module.exports.fetchCourseDetails = async (scrapurl) => {
     // (async () => {
 
-        try {
-            const browser = await puppeteer.launch({ headless: true });
-            const page = await browser.newPage();
-            await page.goto(scrapurl, { waitUntil: 'domcontentloaded' });
-            // await page.waitForSelector(' ')
+    try {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(scrapurl, { waitUntil: 'domcontentloaded' });
+        // await page.waitForSelector(' ')
 
-            let course = await page.$('table[class="light"] > tbody')
-            let courseDetails = await course.$$('tr')
+        let course = await page.$('table[class="light"] > tbody')
+        let courseDetails = await course.$$('tr')
 
-            // console.log(courseDetails.length)
-            const courseData = [] 
+        // console.log(courseDetails.length)
+        const courseData = []
 
-            for (const courseDetail of courseDetails) {
-                const th = await courseDetail.$eval('th', th => th.innerHTML)
-                const td = await courseDetail.$eval('td', td => td.innerHTML)
+        for (const courseDetail of courseDetails) {
+            const th = await courseDetail.$eval('th', th => th.innerHTML)
+            const td = await courseDetail.$eval('td', td => td.innerHTML)
 
-                courseData.push({ th, td })
-                // console.log({ th, td })
-            }
-
-            await browser.close();
-            return courseData
-        } catch (error) {
-            console.log(error)
+            courseData.push({ th, td })
+            // console.log({ th, td })
         }
+
+        await browser.close();
+        return courseData
+    } catch (error) {
+        console.log(error)
+    }
     // })();
 }
 
-const model = {
-    courseId: "CDU001",
-    courseName: "Bachelor of Accounting (WACC01 - 2021)",
-    courseCode: "590251",
-    cricosCode: "060230M",
-    studyArea: "Accounting",
-    courseLevel: "Undergraduate",
-    courseStudyModes: [
+const courseModel = function (
+    courseName,
+    courseCode,
+    coordinatingUnit,
+    level,
+    contact,
+    courseDescription,
+    term,
+    location,
+    prerequisites = "NA",
+    courseLink
+) {
+    this.courseId = uid(5)
+    this.courseName = courseName
+    this.courseCode = courseCode
+    this.cricosCode = "00123M"
+    this.studyArea = coordinatingUnit
+    this.courseLevel = level
+    this.courseStudyModes = [
         {
-            "studyMode": "Full-time",
-            "duration": "3 year/s"
+            studyMode: "Full-time",
+            duration: contact
         }
-    ],
-    totalCreditPoints: "240",
-    courseUnits: [
+    ]
+    this.totalCreditPoints = "NA"
+    this.courseUnits = [
         {
-            "unitType": "Core Units (16 units)",
-            "creditPoints": "160 cp",
-            "description": "",
+            "unitType": "NA",
+            "creditPoints": "NA",
+            "description": courseDescription,
             "unitList": [
                 {
-                    "code": "ACT102",
-                    "title": "ACT102 - Introduction to Accounting",
+                    "code": "NA",
+                    "title": courseName,
                     "year": "2020",
                     "hours": null,
-                    "creditPoints": "10",
+                    "creditPoints": "NA",
                     "semester": [
                         {
                             "year": "2020",
-                            "semester": "Semester 1",
+                            "semester": term,
                             "attendanceMode": "Internal",
-                            "location": "CDU Sydney",
-                            "learningMethod": "OLR"
+                            "location": location,
+                            "learningMethod": "NA"
                         }
                     ],
-                    "sector": "Higher Education",
-                    "discipline": "Accounting and Finance",
-                    "prerequisites": "NA",
-                    "incompatible": "ACT101, CMA101",
+                    "sector": level,
+                    "discipline": coordinatingUnit,
+                    "prerequisites": prerequisites,
+                    "incompatible": "NA",
                     "assumedKnowledge": "NA",
-                    "description": ""
+                    "description": courseDescription
                 },
             ]
-        },
-        {
-            "unitType": "Specialist Elective (0 units)",
-            "creditPoints": "0 cp",
-            "description": "Specialist Elective units are not required for this course.",
-            "unitList": []
-        },
-        {
-            "unitType": "Electives (6 units)",
-            "creditPoints": "60 cp",
-            "description": "Units totalling 60 credit points selected from undergraduate units offered by the University. When selecting electives choose units at a suitable level. A maximum of 20 credit points may be selected at 100 level as this course includes 80 credit points of compulsory 100 level units. Undergraduate units are coded at 100 to 300 level, therefore units coded as ACT1xx, ACT2xx and ACT3xx would be first, second and third year units respectively.",
-            "unitList": []
         }
-    ],
-    isAvailableOnline: true,
-    campuses: [
+    ]
+    this.isAvailableOnline = null
+    this.campuses = [
         {
             "type": "Offline",
-            "campusName": "CDU Waterfront Darwin (CSP)",
+            "campusName": location,
             "postalAddress": null,
             "state": null,
             "geolocation": {
@@ -101,16 +101,75 @@ const model = {
                 "lan": null
             }
         },
-    ],
-    courseFees: [
-        {
-            "type": "Annual Fee",
-            "amount": "$26,776.00",
-            "studenType": "International"
-        }
-    ],
-    institutionSpecificData: {
-        "CDUCode": "WACC01"
-    },
-    courseLink: "https://www.cdu.edu.au/study/bachelor-accounting-wacc01-2021"
+    ]
+    this.courseFees = []
+    this.institutionSpecificData = {
+        "ADUCode": "NA"
+    }
+    this.courseLink = courseLink
 }
+
+// const model = {
+//     courseId: "CDU001",
+//     courseName: "Bachelor of Accounting (WACC01 - 2021)",
+//     courseCode: "590251",
+//     cricosCode: "00123M",
+//     studyArea: "Accounting",
+//     courseLevel: "Undergraduate",
+//     courseStudyModes: [
+//         {
+//             "studyMode": "Full-time",
+//             "duration": "3 year/s"
+//         }
+//     ],
+//     totalCreditPoints: "NA",
+//     courseUnits: [
+//         {
+//             "unitType": "Core Units (16 units)",
+//             "creditPoints": "160 cp",
+//             "description": "",
+//             "unitList": [
+//                 {
+//                     "code": "ACT102",
+//                     "title": "ACT102 - Introduction to Accounting",
+//                     "year": "2020",
+//                     "hours": null,
+//                     "creditPoints": "10",
+//                     "semester": [
+//                         {
+//                             "year": "2020",
+//                             "semester": "Semester 1",
+//                             "attendanceMode": "Internal",
+//                             "location": "CDU Sydney",
+//                             "learningMethod": "OLR"
+//                         }
+//                     ],
+//                     "sector": "Higher Education",
+//                     "discipline": "Accounting and Finance",
+//                     "prerequisites": "NA",
+//                     "incompatible": "ACT101, CMA101",
+//                     "assumedKnowledge": "NA",
+//                     "description": ""
+//                 },
+//             ]
+//         }
+//     ],
+//     isAvailableOnline: null,
+//     campuses: [
+//         {
+//             "type": "Offline",
+//             "campusName": "CDU Waterfront Darwin (CSP)",
+//             "postalAddress": null,
+//             "state": null,
+//             "geolocation": {
+//                 "lat": null,
+//                 "lan": null
+//             }
+//         },
+//     ],
+//     courseFees: [],
+//     institutionSpecificData: {
+//         "ADUCode": "NA"
+//     },
+//     courseLink: "https://www.cdu.edu.au/study/bachelor-accounting-wacc01-2021"
+// }
